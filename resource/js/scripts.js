f9855fd3d1cbe6c120ddeaa44340faee32dfd35f
Edel Abreu <edel.abreu@upr.edu.cu>
2021-06-13 11:28:45 -0400

    update user interface and update translations

--- resource/js/scripts.js
+++ resource/js/scripts.js
@@ -478,3 +478,22 @@ function loadMappingProperties(concept, lang, contentLang, $htmlElement, concept
     }
   });
 }
+
+function createRipple(event){
+  const button = event.currentTarget;
+  const circle = document.createElement('span');
+  const diameter = Math.max(button.clientWidth, button.clientHeight);
+  const radius = diameter/2;
+  // circle.style.width = circle.style.height = `${diameter}px`;
+  // circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+  // circle.style.top - `${event.clientY - button.offsetTop - radius}px`;
+  circle.classList.add('ripple');
+
+  const ripple = button.getElementsByClassName('ripple')[0];
+  if (ripple) {
+    ripple.remove();
+  }
+  button.appendChild(circle);
+}
+const button = document.getElementById('dropdownMenuLink');
+button.addEventListener('click', createRipple);
